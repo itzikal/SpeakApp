42c60e86e4f0f4cb09b349ccd716184183f9d95a
itzik <thespeakpicgroup@gmail.com>
2014-06-26 16:24:09 +0300

    press setting to enter settings mode. intreducing settings activity

--- speakapp/src/main/java/com/speakapp/activites/EnableSettingActivity.java
+++ speakapp/src/main/java/com/speakapp/activites/EnableSettingActivity.java
@@ -1,8 +1,10 @@
 package com.speakapp.activites;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.speakapp.speakapp.R;
 
@@ -16,12 +18,26 @@ public class EnableSettingActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enable_settings);
-        findViewById(R.id.cancel_setting_mode).setOnClickListener(new View.OnClickListener() {
+        final EditText enableSettingTextView = (EditText)findViewById(R.id.enable_setting_text_view);
+        findViewById(R.id.cancel_setting_mode).setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View view)
             {
                 finish();
             }
         });
+        findViewById(R.id.go_to_settings).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                if(enableSettingTextView.getText().toString().toLowerCase().equals("settings"))
+                {
+                    startActivity(new Intent(EnableSettingActivity.this, SettingsActivity.class));
+                    finish();
+                }
+            }
+        });
+
     }
 }
---
+++ speakapp/src/main/java/com/speakapp/activites/SettingsActivity.java
@@ -0,0 +1,19 @@
+package com.speakapp.activites;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.speakapp.speakapp.R;
+
+/**
+ * Created by Itzik on 26/06/2014.
+ */
+public class SettingsActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
